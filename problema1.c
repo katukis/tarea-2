be4caf04da1e65b8d293535b5c9a475260638c2e
katukis <katukis@outlook.com>
2025-11-19 16:18:59 -0600

    Create problema1.c

---
+++ problema1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    float a, b;
+    float suma, resta, multiplicacion;
+
+    printf("Dame el primer numero: ");
+    scanf("%f", &a);
+    printf("Dame el segundo numero: ");
+    scanf("%f", &b);
+
+    suma = a + b;
+    resta = a - b;
+    multiplicacion = a * b;
+
+    printf("Suma: %.2f\n", suma);
+    printf("Resta: %.2f\n", resta);
+    printf("Multiplicacion: %.2f\n", multiplicacion);
+
+    return 0;
+}

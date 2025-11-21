c6082cfb2d90ae3df56bf150e587fee9f023edfa
katukis <katukis@outlook.com>
2025-11-20 22:51:30 -0600

    Update problema2.c

--- problema2.c
+++ problema2.c
@@ -4,18 +4,11 @@ int main() {
     float radio;
     float area, circunferencia;
     const float PI = 3.141592f;
-
-    // Entrada
     printf("Dame el radio del circulo: ");
     scanf("%f", &radio);
-
-    // Proceso
     area = PI * radio * radio;
     circunferencia = 2 * PI * radio;
-
-    // Salida
     printf("Area: %.4f\n", area);
     printf("Circunferencia: %.4f\n", circunferencia);
-
     return 0;
 }
